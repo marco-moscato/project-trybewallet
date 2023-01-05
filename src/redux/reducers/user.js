71e055d23cee8fe347f35c7820d01dfee7f84983
Marco Moscato <marcomoscatto@gmail.com>
2023-01-05 14:42:34 -0300

    Configuração inicial

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,12 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+  username: [],
+};
+
+const userReducer = (state = initialState, action) => {
+  console.log('State: ', state);
+  console.log('Action: ', action);
+  return state;
+};
+
+export default userReducer;
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,12 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  wallet: [],
+};
+
+const walletReducer = (state = initialState, action) => {
+  console.log('State: ', state);
+  console.log('Action: ', action);
+  return state;
+};
+
+export default walletReducer;
