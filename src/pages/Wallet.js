5e6245cb61190e54cb7400bbe3087957603e25ee
Marco Moscato <marcomoscatto@gmail.com>
2023-01-15 08:51:35 -0300

    feat: cria o header conforme requisito 2.

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
 
-export default Wallet;
+export default connect()(Wallet);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,11 +1,11 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-const initialState = {
+const INITIAL_STATE = {
   wallet: [],
+  expenses: 0,
 };
 
-const walletReducer = (state = initialState, action) => {
-  console.log('State: ', state);
-  console.log('Action: ', action);
+const walletReducer = (state = INITIAL_STATE, action) => {
+  console.log(state);
   return state;
 };
 
