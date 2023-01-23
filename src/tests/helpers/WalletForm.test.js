467f5bc0f16a7abecf0d5022b22381963b245d66
Marco Moscato <marcomoscatto@gmail.com>
2023-01-23 18:28:09 -0300

    fix: correções de lint

--- src/tests/helpers/WalletForm.test.js
+++ src/tests/helpers/WalletForm.test.js
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Wallet from '../../pages/Wallet';
 import { renderWithRouterAndRedux } from './renderWith';
@@ -15,7 +15,7 @@ describe('', () => {
       const currency = screen.getByTestId('currency-input');
       const method = screen.getByTestId('method-input');
       const tag = screen.getByTestId('tag-input');
-      const button = screen.getByRole('button', { name: /despesa/i });
+      // const button = screen.getByRole('button', { name: /despesa/i });
 
       expect(value).toBeDefined();
       expect(value.type).toBe('number');
@@ -26,38 +26,38 @@ describe('', () => {
       expect(tag).toBeDefined();
     });
 
-    test('Test if is rendered an e-mail and password input', () => {
-      renderWithRouterAndRedux(<Wallet />);
+    // test('Test if is rendered an e-mail and password input', () => {
+    //   renderWithRouterAndRedux(<Wallet />);
 
-      const value = screen.getByTestId('value-input');
-      const description = screen.getByTestId('description-input');
-      const currency = screen.getByTestId('currency-input');
-      const method = screen.getByTestId('method-input');
-      const tag = screen.getByTestId('tag-input');
-      const button = screen.getByRole('button', { name: /despesa/i });
+    //   const value = screen.getByTestId('value-input');
+    //   const description = screen.getByTestId('description-input');
+    //   const currency = screen.getByTestId('currency-input');
+    //   const method = screen.getByTestId('method-input');
+    //   const tag = screen.getByTestId('tag-input');
+    //   const button = screen.getByRole('button', { name: /despesa/i });
 
-      expect(value).toBeDefined();
-      expect(value.type).toBe('number');
-      expect(description).toBeDefined();
-      expect(description.type).toBe('text');
-      expect(currency).toBeDefined();
-      expect(method).toBeDefined();
-      expect(tag).toBeDefined();
+    //   expect(value).toBeDefined();
+    //   expect(value.type).toBe('number');
+    //   expect(description).toBeDefined();
+    //   expect(description.type).toBe('text');
+    //   expect(currency).toBeDefined();
+    //   expect(method).toBeDefined();
+    //   expect(tag).toBeDefined();
 
-      // userEvent.type(value, '11');
-      // userEvent.type(description, 'restaurante');
-      // userEvent.click(button);
+    //   // userEvent.type(value, '11');
+    //   // userEvent.type(description, 'restaurante');
+    //   // userEvent.click(button);
 
-      // const table = screen.getByRole('table');
-      // expect(table).toBeDefined();
+    //   // const table = screen.getByRole('table');
+    //   // expect(table).toBeDefined();
 
-      // const editButton = screen.getByTestId('edit-btn');
-      // // const info = screen.getAllByRole('row', { })
-      // expect(editButton).toBeDefined();
-      // userEvent.click(editButton);
+    //   // const editButton = screen.getByTestId('edit-btn');
+    //   // // const info = screen.getAllByRole('row', { })
+    //   // expect(editButton).toBeDefined();
+    //   // userEvent.click(editButton);
 
-      // const addToEdit = screen.findByRole('button', { name: /editar despesa/i });
-      // expect(addToEdit).tobeDefined();
-    });
+    //   // const addToEdit = screen.findByRole('button', { name: /editar despesa/i });
+    //   // expect(addToEdit).tobeDefined();
+    // });
   });
 });
